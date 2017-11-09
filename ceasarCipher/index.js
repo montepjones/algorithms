76f375b05684b52956e47b5dc7f02b52666c8293
Monte Jones <mjones@VPayUSA.net>
2017-11-09 14:27:28 -0600

    Ceasar Cipher algorith

---
+++ ceasarCipher/index.js
@@ -0,0 +1,29 @@
+function ceasarCipher (str, num) {
+    num = num % 26;
+    let lowerCaseString = str.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let newString = '';
+    for (let i = 0; i < lowerCaseString.length; i++){
+        let currentLetter = lowerCaseString[i];
+        if (currentLetter === ' ') {
+           newString += currentLetter;
+           continue; 
+        }
+        let currentIndex = alphabet.indexOf(currentLetter);
+        let newIndex = currentIndex + num;
+        if (newIndex > 25) newIndex = newIndex - 26;
+        if (newIndex < 0) newIndex = 26 + newIndex;
+        if (str[i] === str[i].toUpperCase()) {
+            newString += alphabet[newIndex].toUpperCase();
+
+        }
+        else {
+            newString += alphabet[newIndex];
+        }
+    }
+    return newString;
+}
+
+console.log(ceasarCipher('Zoo Keeper', 2));  
+console.log(ceasarCipher('Javascript', -900));  
+
